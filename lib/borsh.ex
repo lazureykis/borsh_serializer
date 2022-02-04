08bfc21e297ff500d99df6855fb8fe132352fbde
Pavel Lazureykis <pavel@lazureykis.dev>
2022-02-04 17:28:51 +0300

    binary and floats

--- lib/borsh.ex
+++ lib/borsh.ex
@@ -1,3 +1,111 @@
+defmodule Person do
+  defstruct id: 123, name: "John", email: "john@gmail.com"
+
+  def borsh_schema do
+    [
+      {:id, :u16},
+      {:name, :string},
+      {:email, :string}
+    ]
+  end
+end
+
+defmodule Creator do
+  defstruct address: [],
+            share: 0,
+            verified: 0
+
+  def borsh_schema do
+    [
+      {:address, {:binary, _bytes = 32}},
+      {:verified, :u8},
+      {:share, :u8}
+    ]
+  end
+
+  # [
+  #   Creator,
+  #   {
+  #     kind: 'struct',
+  #     fields: [
+  #       ['address', 'pubkeyAsString'],
+  #       ['verified', 'u8'],
+  #       ['share', 'u8'],
+  #     ],
+  #   },
+  # ],
+end
+
+defmodule Data do
+  defstruct name: "",
+            symbol: "",
+            uri: "",
+            seller_fee_basis_points: 0,
+            creators: []
+
+  def borsh_schema do
+    [
+      {:name, :string},
+      {:symbol, :string},
+      {:uri, :string},
+      {:seller_fee_basis_points, :u16},
+      {:creators, {:option, {:array, Creator}}}
+    ]
+  end
+
+  # [
+  #   Data,
+  #   {
+  #     kind: 'struct',
+  #     fields: [
+  #       ['name', 'string'],
+  #       ['symbol', 'string'],
+  #       ['uri', 'string'],
+  #       ['sellerFeeBasisPoints', 'u16'],
+  #       ['creators', { kind: 'option', type: [Creator] }],
+  #     ],
+  #   },
+  # ],
+end
+
+defmodule Metadata do
+  defstruct key: 0,
+            authority: [],
+            update_authority: [],
+            mint: [],
+            data: %Data{},
+            primary_sale_happened: 0,
+            is_mutable: 0,
+            edition_nonce: nil
+
+  def borsh_schema do
+    [
+      {:key, :u8},
+      {:update_authority, {:binary, 256}},
+      {:mint, {:binary, 256}},
+      {:data, Data},
+      {:primary_sale_happened, :u8},
+      {:is_mutable, :u8},
+      {:edition_nonce, {:option, :u8}}
+    ]
+  end
+
+  #
+  # Metadata,
+  #   {
+  #     kind: 'struct',
+  #     fields: [
+  #       ['key', 'u8'],
+  #       ['updateAuthority', 'pubkeyAsString'],
+  #       ['mint', 'pubkeyAsString'],
+  #       ['data', Data],
+  #       ['primarySaleHappened', 'u8'], // bool
+  #       ['isMutable', 'u8'], // bool
+  #       ['editionNonce', { kind: 'option', type: 'u8' }],
+  #     ],
+  #   },
+end
+
 defmodule Borsh do
   @moduledoc """
   Documentation for `Borsh`.
@@ -15,16 +123,25 @@ defmodule Borsh do
   def hello do
     :world
   end
-end
 
-defmodule MyStruct do
-  defstruct id: 0, name: "John", email: "Doe"
-end
+  def test_encoder_with_person do
+    person = %Person{} |> IO.inspect(label: "person")
 
-@schema = %{
-  MyStruct => [
-    {:id, :u16},
-    {:name, :string},
-    {:email, :string}
-  ]
-}
+    Borsh.Encoder.encode(person)
+    |> IO.inspect(label: "DATA")
+  end
+
+  def test_encoder_with_creator do
+    address =
+      Base58.decode("D1yTsfytXgUFaiHh9gFJXPwxFFSvV63XVFN4ti2C56nf")
+      |> IO.inspect(label: "Metadata address")
+
+    IO.puts("byte_size(address) #{byte_size(address)}")
+
+    creator =
+      %Creator{address: address, share: 100_000, verified: 0} |> IO.inspect(label: "creator")
+
+    Borsh.Encoder.encode(creator)
+    |> IO.inspect(label: "DATA")
+  end
+end
--- lib/borsh/encoder.ex
+++ lib/borsh/encoder.ex
@@ -1,4 +1,77 @@
 defmodule Borsh.Encoder do
-  def encode(struct, schema) do
+  def encode(struct) when is_struct(struct) do
+    struct.__struct__ |> IO.inspect(label: "struct.__struct__")
+
+    struct_schema = apply(struct.__struct__, :borsh_schema, [])
+    # struct_schema = full_schema |> Map.fetch!(struct.__struct__)
+
+    struct_schema
+    |> Enum.reduce(<<>>, fn field_def, acc ->
+      {field_name, type} = field_def |> IO.inspect(label: "field schema")
+      field_name |> IO.inspect(label: "field_name")
+      field_data = Map.fetch!(struct, field_name) |> IO.inspect(label: "field_data")
+
+      acc <> encode_field(type, field_data)
+    end)
+  end
+
+  # Unsigned
+
+  def encode_field(:u8, num) do
+    <<num::little-integer-size(8)>>
+  end
+
+  def encode_field(:u16, num) do
+    <<num::little-integer-size(16)>>
+  end
+
+  def encode_field(:u32, num) do
+    <<num::little-integer-size(32)>>
+  end
+
+  def encode_field(:u64, num) do
+    <<num::little-integer-size(64)>>
+  end
+
+  # Integer
+
+  def encode_field(:i8, num) do
+    <<num::little-integer-signed-size(8)>>
+  end
+
+  def encode_field(:i16, num) do
+    <<num::little-integer-signed-size(16)>>
+  end
+
+  def encode_field(:i32, num) do
+    <<num::little-integer-signed-size(32)>>
+  end
+
+  def encode_field(:i64, num) do
+    <<num::little-integer-signed-size(64)>>
+  end
+
+  # Float
+
+  def encode_field(:f32, num) do
+    <<num::little-float-size(32)>>
+  end
+
+  def encode_field(:f64, num) do
+    <<num::little-float-size(64)>>
+  end
+
+  # String
+
+  def encode_field(:string, string) do
+    # :erlang.binary_to_list(string)
+    len = byte_size(string)
+    <<len::little-integer-size(32), string::binary>>
+  end
+
+  # Binary
+
+  def encode_field({:binary, len}, data) do
+    <<data::binary-size(len)>>
   end
 end
